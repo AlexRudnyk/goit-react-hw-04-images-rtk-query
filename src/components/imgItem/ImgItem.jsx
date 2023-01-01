464d3a52adc97e5578e947d793b6a1a2f5f83ff1
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-01-01 20:14:41 +0200

    Modal works

--- src/components/imgItem/ImgItem.jsx
+++ src/components/imgItem/ImgItem.jsx
@@ -1,9 +1,23 @@
+import { Modal } from 'components/modal';
+import { useState } from 'react';
 import { Item, Image } from './ImgItem.styled';
 
 export const ImgItem = ({ webFormat, alt, largeImage }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
   return (
-    <Item>
-      <Image src={webFormat} alt={alt} loading="lazy" />
-    </Item>
+    <>
+      <Item onClick={toggleModal}>
+        <Image src={webFormat} alt={alt} loading="lazy" />
+      </Item>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <Image src={largeImage} alt={alt} loading="lazy" />
+        </Modal>
+      )}
+    </>
   );
 };
