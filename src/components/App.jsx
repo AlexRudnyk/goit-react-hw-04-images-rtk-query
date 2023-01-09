8413e00ba21b42600170306ea89f2bcdf105230f
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-01-09 10:02:50 +0200

    Deleted log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,8 +25,6 @@ export const App = () => {
     setSearchPage(state => state + 1);
   };
 
-  console.log('QUERY: ', searchQuery);
-
   return (
     <>
       <SearchBar onSubmit={handleSubmit} />
